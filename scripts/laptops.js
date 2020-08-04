e22388e706b4c6a6cbfc19e709f678e19d1e3a69
MarkoSelchanec <59167796+MarkoSelchanec@users.noreply.github.com>
2020-08-04 23:14:34 +0200

    added pagination

--- scripts/laptops.js
+++ scripts/laptops.js
@@ -1,16 +1,211 @@
-let lapTopsData = [];
 class laptop {
-    constructor(img, name, price) {
-      this.img = img;
-      this.name = name;
-      this.price = price;
-    }
+  constructor(img, name, price) {
+    this.img = img;
+    this.name = name;
+    this.price = price;
   }
+}
 
-  export const lenovoThinkPadd = new laptop(
+const lenovoThinkPadd = new laptop(
+  'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+  'Lenovo - ThinkPad',
+  `800$`
+);
+
+export let lapTopsData = [
+  new laptop(
     'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
-    'Lenovo - ThinkPad',
+    'Lenovo - ThinkPad1',
     `800$`
-  );
-
-
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad2',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad3',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad4',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad5',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad6',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad7',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad8',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad9',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad10',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad11',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad12',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad13',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad14',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad15',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad16',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad17',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad18',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad19',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad20',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad21',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad22',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad23',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad24',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad25',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad26',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad27',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad28',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad29',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad30',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad31',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad32',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad33',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad34',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad35',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad36',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad37',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad38',
+    `800$`
+  ),
+  new laptop(
+    'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
+    'Lenovo - ThinkPad39',
+    `800$`
+  ),
+];
--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,107 @@
-import { lenovoThinkPadd } from './laptops.js';
-
+import { lapTopsData } from './laptops.js';
 
 $(document).ready(function () {
-  
-  // let lapTopsData = [];
-
-  // class laptop {
-  //   constructor(img, name, price) {
-  //     this.img = img;
-  //     this.name = name;
-  //     this.price = price;
-  //   }
-  // }
-
-  // const lenovoThinkPadd = new laptop(
-  //   'https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t490-hero-1126.png?context=bWFzdGVyfHJvb3R8Nzg4OTN8aW1hZ2UvcG5nfGg5Ni9oYTcvMTA2NzMyMDYyNjM4MzgucG5nfDllOTUyNGM1OGYxNjkxNGU2ZTMxNWQxMTE1NDQ0MGZkNDU4ZWYwOWExZWZjMDJkMDRiZTIwYjc0OGE5NGUwMzA',
-  //   'Lenovo - ThinkPad',
-  //   `800$`
-  // );
-
-  const pageCreate = (row1, row2, row3, row4) => {
+  const pageCreate = (array, page) => {
     const container = document.createElement('div');
-    $(container).addClass('m-5');
-    $(container).append(row1);
-    $(container).append(row2);
-    $(container).append(row3);
-    $(container).append(row4);
+    $(container).addClass(' container container-cards my-5 mx-auto');
+    $(container).append(array[page]);
+    $('#paginate-prev').removeClass('disabled');
+    if (page === 0) {
+      $('#paginate-prev').addClass('disabled');
+    }
+    $('#paginate-next').removeClass('disabled');
+
+    if (page === array.length - 1) {
+      $('#paginate-next').addClass('disabled');
+    }
+    for (let i = 0; i < $('.pagination').children().length; i++) {
+      $('.pagination').children()[i].classList.remove('active');
+    }
+
+    $('.pagination').children()[page + 1].classList.add('active');
+    $('#paginate-next').removeClass('active');
     return container;
   };
 
-  const rowCreate = (obj1, obj2, obj3, obj4) => {
-    const container = document.createElement('div');
-    const row = document.createElement('div');
-    const column1 = document.createElement('div');
-    const column2 = document.createElement('div');
-    const column3 = document.createElement('div');
-    const column4 = document.createElement('div');
-    $(container).addClass('container container-cards my-auto').append(row);
-    $(row).addClass('row d-flex justify-content-center');
-    $(column1).addClass('col-xs').append(obj1);
-    $(column2).addClass('col-xs').append(obj2);
-    $(column3).addClass('col-xs').append(obj3);
-    $(column4).addClass('col-xs').append(obj4);
-    $(row).append(column1).append(column2).append(column3).append(column4);
-    return container;
+  const paginate = (array) => {
+    $('#paginate').addClass('d-flex').removeClass('d-none');
+    let currentPage = 0;
+    $('.insert-page').append(pageCreate(rowCreate(cardArray), currentPage));
+    for (let i = 0; i < array.length; i++) {
+      let li = document.createElement('li');
+      let a = document.createElement('a');
+      $(li).addClass('page-item');
+      $(a)
+        .addClass('page-link')
+        .attr('href', '#')
+        .text(i + 1)
+        .click((e) => {
+          e.preventDefault();
+          $('.insert-page').empty();
+          currentPage = i;
+          $('.insert-page').append(
+            pageCreate(rowCreate(cardArray), currentPage)
+          );
+        });
+      console.log(array);
+      $(li).append(a);
+      $(li).insertBefore('#paginate-next');
+    }
+    $('#paginate-next').click((e) => {
+      e.preventDefault();
+      if (currentPage <= array.length - 2) {
+        $('.insert-page').empty();
+        $('.insert-page').append(
+          pageCreate(rowCreate(cardArray), currentPage + 1)
+        );
+        currentPage++;
+        $('.pagination').children()[currentPage].classList.remove('active');
+        $('.pagination').children()[currentPage + 1].classList.add('active');
+      }
+    });
+    $('#paginate-prev').click((e) => {
+      e.preventDefault();
+      if (currentPage !== 0) {
+        $('.insert-page').empty();
+        $('.insert-page').append(
+          pageCreate(rowCreate(cardArray), currentPage - 1)
+        );
+        currentPage--;
+        $('.pagination').children()[currentPage + 2].classList.remove('active');
+        $('.pagination').children()[currentPage + 1].classList.add('active');
+      }
+    });
+    $('.pagination').children()[currentPage + 1].classList.add('active');
+  };
+
+  const rowCreate = (array) => {
+    // split array into chunks
+    let chunk = 12; // items per chunk
+
+    let result = array.reduce((resultArray, item, index) => {
+      const chunkIndex = Math.floor(index / chunk);
+      if (!resultArray[chunkIndex]) {
+        resultArray[chunkIndex] = []; // start a new chunk
+      }
+      resultArray[chunkIndex].push(item);
+      return resultArray;
+    }, []);
+
+    let column = '';
+    let row = '';
+    let tempArr = [];
+    for (const arr of result) {
+      row = document.createElement('div');
+      $(row).addClass('row d-flex justify-content-center');
+      for (const a of arr) {
+        column = document.createElement('div');
+        $(column).addClass('col-xs');
+        $(column).append(a);
+        $(row).append(column);
+      }
+      tempArr.push(row);
+    }
+    return tempArr;
   };
 
   const cardCreate = (object) => {
@@ -65,32 +123,12 @@ $(document).ready(function () {
     $(body).append(cardTitle).append(cardText).append(cardBtn);
     return card;
   };
-  $(
-    pageCreate(
-      rowCreate(
-        cardCreate(lenovoThinkPadd),
-        cardCreate(lenovoThinkPadd),
-        cardCreate(lenovoThinkPadd),
-        cardCreate(lenovoThinkPadd)
-      ),
-      rowCreate(
-        cardCreate(lenovoThinkPadd),
-        cardCreate(lenovoThinkPadd),
-        cardCreate(lenovoThinkPadd),
-        cardCreate(lenovoThinkPadd)
-      ),
-      rowCreate(
-        cardCreate(lenovoThinkPadd),
-        cardCreate(lenovoThinkPadd),
-        cardCreate(lenovoThinkPadd),
-        cardCreate(lenovoThinkPadd)
-      ),
-      rowCreate(
-        cardCreate(lenovoThinkPadd),
-        cardCreate(lenovoThinkPadd),
-        cardCreate(lenovoThinkPadd),
-        cardCreate(lenovoThinkPadd)
-      )
-    )
-  ).insertBefore('.insert-before');
+
+  let cardArray = [];
+  const createCardArray = (array) => {
+    array.forEach((e) => cardArray.push(cardCreate(e)));
+  };
+  createCardArray(lapTopsData);
+
+  paginate(rowCreate(cardArray));
 });
